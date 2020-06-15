fa2c3bc5cb45c7e935e54f85338c958967de10ec
Luke Kaalim <lkaalim@nine.com.au>
2020-06-16 01:15:28 +1000

    Allow providers to include name

--- src/terraform-helper/provider.js
+++ src/terraform-helper/provider.js
@@ -1,9 +1,11 @@
 const createProvider = ({
+  name,
   version = 1,
   schema,
   prepare = (config) => config,
   configure = (config) => null,
 }) => ({
+  name,
   version,
   schema,
   prepare,
